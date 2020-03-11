ea798ce22366cd6c0af2151f1e0685d094951328
Keith Bennett <keithrbennett@gmail.com>
2020-03-12 00:51:44 +0800

    Fix which files are loaded when the gem is required.

--- lib/simple_xml_parser.rb
+++ lib/simple_xml_parser.rb
@@ -1,6 +1,13 @@
 require "simple_xml_parser/version"
 
 module SimpleXmlParser
+
+  lib_dir = File.dirname(__FILE__)
+  file_mask = File.join(lib_dir, '**', '*.rb')
+  Dir[file_mask].each do |ruby_file|
+    require ruby_file
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
+
 end
